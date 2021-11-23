8cecb5181ac4df063176bb0f5d74de1d9272644b
yunxi.zhang <yunxi.zhang@homtail.com>
2021-11-23 20:27:13 +0000

    update name from Alice to issuer

--- issuer/src/app/components/app/app.component.spec.ts
+++ issuer/src/app/components/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'alice-controller'`, () => {
+  it(`should have as title 'issuer-controller'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('alice-controller');
+    expect(app.title).toEqual('issuer-controller');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to alice-controller!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to issuer-controller!');
   });
 });
--- issuer/src/app/components/app/app.component.ts
+++ issuer/src/app/components/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'alice-controller';
+  title = 'issuer-controller';
 }
