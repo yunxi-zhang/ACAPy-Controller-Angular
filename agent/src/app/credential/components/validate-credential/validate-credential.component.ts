9bde2879e60c4493994af9f5e57aebb1f7977e15
yunxi.zhang <yunxi.zhang@homtail.com>
2022-01-22 17:46:06 +0000

    update credential validity checking function to include non-revoked credentials

--- agent/src/app/credential/components/validate-credential/validate-credential.component.ts
+++ agent/src/app/credential/components/validate-credential/validate-credential.component.ts
@@ -3,14 +3,15 @@ import { AgentService } from '../../../services/agent.service';
 import { map } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-revoked-credential',
-  templateUrl: './revoked-credential.component.html',
-  styleUrls: ['./revoked-credential.component.scss']
+  selector: 'app-validate-credential',
+  templateUrl: './validate-credential.component.html',
+  styleUrls: ['./validate-credential.component.scss']
 })
 export class ValidateCredentialComponent implements OnInit {
   connectionID: any;
   revokedCredentialExchangeIDs: any[] = [];
   revokedCredentialExchangeID: any;
+  AVAIL_SUFFIX: string = "_available";
   REVOKED_SUFFIX: string = "_revoked";
   checkRevokedCredentialResultObject: any;
 
@@ -29,10 +30,10 @@ export class ValidateCredentialComponent implements OnInit {
     .subscribe();
   }
 
-  //get credential exchange ID from a list with a key: $connectionID_available in the local storage 
-  //the list shows all credential exchange IDs related to non-revoked credentials
+  //get credential exchange ID from two lists with their respective key: $connectionID_available and $connectionID_revoked in the local storage 
+  //the list shows all credential exchange IDs related to credentials issued by the issuer regardless of whether they've been revoked or not
   getCredentialExchangeIDs(connectionID) {
-    this.revokedCredentialExchangeIDs = JSON.parse(localStorage.getItem(connectionID + this.REVOKED_SUFFIX));
+    this.revokedCredentialExchangeIDs = JSON.parse(localStorage.getItem(connectionID + this.AVAIL_SUFFIX)).concat(JSON.parse(localStorage.getItem(connectionID + this.REVOKED_SUFFIX)));
   }
 
   onSubmit() {
