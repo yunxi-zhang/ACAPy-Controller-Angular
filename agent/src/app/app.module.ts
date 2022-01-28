b356ab89335cf9a50b40509fdea9111331b80d52
yunxi.zhang <yunxi.zhang@homtail.com>
2022-01-28 16:26:50 +0000

    add basic i18n function

--- agent/src/app/app.module.ts
+++ agent/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './components/app/app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { NavCardComponent } from './components/nav-card/nav-card.component';
 import { NavCardListComponent } from './components/nav-card-list/nav-card-list.component';
-
 import { InterceptorService } from './services/interceptor.service';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -23,7 +26,15 @@ import { InterceptorService } from './services/interceptor.service';
   imports: [
     BrowserModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true },
--- agent/src/app/components/nav/nav.component.ts
+++ agent/src/app/components/nav/nav.component.ts
@@ -4,6 +4,7 @@ import { AgentService } from 'src/app/services/agent.service';
 import { AgentStatus } from 'src/app/enums/agent-status.enum';
 
 import { map } from 'rxjs/operators';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-nav',
@@ -14,7 +15,11 @@ export class NavComponent implements OnInit {
   agentStatus = AgentStatus;
   status = this.agentStatus.Loading;
 
-  constructor(private agentService: AgentService) { }
+  constructor(private agentService: AgentService, public translate: TranslateService) { 
+    translate.addLangs(['en']);
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
 
   ngOnInit() {
     this.agentService.getStatus()
