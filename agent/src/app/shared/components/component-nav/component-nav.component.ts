92b7f4496229437899e2f405288b985fbc3dd827
yunxi.zhang <yunxi.zhang@homtail.com>
2022-02-01 15:53:55 +0000

    add language support home menu

--- agent/src/app/shared/components/component-nav/component-nav.component.ts
+++ agent/src/app/shared/components/component-nav/component-nav.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
 import { NavLink } from 'src/app/models/nav-link';
-
 import { NavLinkService } from 'src/app/services/nav-link.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-component-nav',
@@ -12,10 +11,15 @@ import { NavLinkService } from 'src/app/services/nav-link.service';
 export class ComponentNavComponent implements OnInit {
   moreNavLinks: NavLink[] = [];
 
-  constructor(private navLinkService: NavLinkService) { }
+  constructor(private navLinkService: NavLinkService, private translateService: TranslateService) { }
 
   ngOnInit() {
+    //triggered when a user visits the website for the 1st time
     this.moreNavLinks = this.navLinkService.getNavLinks();
+    //triggered when a user choose another language
+    this.translateService.onLangChange.subscribe(() => {
+      this.moreNavLinks = this.navLinkService.getNavLinks();
+    });
   }
 
 }
--- agent/src/app/shared/shared.module.ts
+++ agent/src/app/shared/shared.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { ComponentNavComponent } from './components/component-nav/component-nav.component';
 import { EmptyListComponent } from './components/empty-list/empty-list.component';
-
 import { ToDatePipe } from './pipes/to-date.pipe';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ComponentNavComponent, EmptyListComponent, ToDatePipe],
   imports: [
     CommonModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    TranslateModule
   ],
   exports: [
     CommonModule,
