9dedc3abbbe70e1e8f5c63f38efb8e7db41688de
yunxi.zhang <yunxi.zhang@hotmail.com>
2022-08-20 15:08:27 +0100

    complete msal login

--- agent/src/app/components/nav/nav.component.ts
+++ agent/src/app/components/nav/nav.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { AgentService } from 'src/app/services/agent.service';
 import { AgentStatus } from 'src/app/enums/agent-status.enum';
-import { map } from 'rxjs/operators';
+import { filter, takeUntil, map } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 import { MsalService, MsalBroadcastService, MSAL_GUARD_CONFIG, MsalGuardConfiguration } from '@azure/msal-angular';
-import { AuthenticationResult, InteractionStatus, InteractionType, PopupRequest, RedirectRequest } from '@azure/msal-browser';
+import { AuthenticationResult, InteractionStatus, InteractionType, PopupRequest, RedirectRequest, EventMessage, EventType } from '@azure/msal-browser';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -16,6 +17,10 @@ export class NavComponent implements OnInit {
   status = this.agentStatus.Loading;
   selectedLanguage: string;
   loginDisplay = false;
+  isIframe = false;
+  private readonly _destroying$ = new Subject<void>();
+  dataSource: any =[];
+  name: string;
 
   constructor(
     private agentService: AgentService, 
@@ -34,6 +39,27 @@ export class NavComponent implements OnInit {
         map((status) => this.status = status)
       )
       .subscribe();
+    
+    this.msalBroadcastService.msalSubject$
+      .pipe(
+        filter((msg: EventMessage) => msg.eventType === EventType.LOGIN_SUCCESS),
+        takeUntil(this._destroying$)
+      )
+      .subscribe((result: EventMessage) => {
+        console.log('result:',result);
+        const payload = result.payload as AuthenticationResult;
+        this.authService.instance.setActiveAccount(payload.account);
+      });
+
+      this.msalBroadcastService.inProgress$
+      .pipe(
+        filter((status: InteractionStatus) => status === InteractionStatus.None)
+      )
+      .subscribe(() => {
+        this.setLoginDisplay();
+        this.checkAndSetActiveAccount();
+        this.getClaims(this.authService.instance.getActiveAccount()?.idTokenClaims)
+      });
   }
 
   onChange(value) {
@@ -42,12 +68,11 @@ export class NavComponent implements OnInit {
 
   setLoginDisplay() {
     this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
-    console.log('display:', this.loginDisplay);
   }
 
   login() {
     if (this.msalGuardConfig.interactionType === InteractionType.Popup) {
-      if (this.msalGuardConfig.authRequest){
+      if (this.msalGuardConfig.authRequest){     
         this.authService.loginPopup({...this.msalGuardConfig.authRequest} as PopupRequest)
           .subscribe((response: AuthenticationResult) => {
             this.authService.instance.setActiveAccount(response.account);
@@ -69,5 +94,31 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.authService.logout();
+  }
+
+  // unsubscribe to events when component is destroyed
+  ngOnDestroy(): void {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
+  }
+
+  checkAndSetActiveAccount() {
+    /**
+     * If no active account set but there are accounts signed in, sets first account to active account
+     * To use active account set here, subscribe to inProgress$ first in your component
+     * Note: Basic usage demonstrated. Your app may require more complicated account selection logic
+     */
+    let activeAccount = this.authService.instance.getActiveAccount();
+
+    if (!activeAccount && this.authService.instance.getAllAccounts().length > 0) {
+      let accounts = this.authService.instance.getAllAccounts();
+      this.authService.instance.setActiveAccount(accounts[0]);
+    }
+  }
+
+  getClaims(claims: any) {
+    this.dataSource = [
+      {id: 1, claim: "Display Name", value: claims ? claims['name'] : null}
+    ];
   }
 }
